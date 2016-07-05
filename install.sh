0b6e065ced7136bd2c19585e928506286f787c94
Chad Engler <englercj@live.com>
2016-07-04 17:52:20 -0700

    update install script

--- install.sh
+++ install.sh
@@ -8,23 +8,27 @@ DIR="$TEMP/minecraft-scripts"
 REPO="https://github.com/englercj/minecraft.git"
 
 # Clone to temp dir
-echo "Downloading from git to '$DIR'" &&
-mkdir -p "$DIR" &&
-command git clone "$REPO" "$DIR" || echo >&2 "Failed to clone repository. Please report this!" && exit 1
+clone_repo() {
+    echo "Downloading from git to '$DIR'" &&
+    mkdir -p "$DIR" &&
+    command git clone "$REPO" "$DIR"
+}
 
 # move into temp directory
-echo "Installing files..." &&
-cd "$DIR" &&
-mkdir /etc/minecraft &&
-mv -v ./default/* /etc/default/ &&
-mv -v ./init.d/* /etc/* &&
-mv -v ./minecraft /etc/ &&
+copy_files() {
+    echo "Installing files..." &&
+    cd "$DIR" &&
+    mkdir /etc/minecraft &&
+    mv -v ./default/* /etc/default/ &&
+    mv -v ./init.d/* /etc/* &&
+    mv -v ./minecraft /etc/ &&
 
-echo "Setting permissions..." &&
-chown $USER /etc/minecraft &&
-chown $USER /etc/minecraft/* &&
-chown $USER /etc/default/minecraft &&
+    echo "Setting permissions..." &&
+    chown $USER /etc/minecraft &&
+    chown $USER /etc/minecraft/* &&
+    chown $USER /etc/default/minecraft
+}
 
-echo "Installed!"
+clone_repo && copy_files && echo "Installed!"
 
 } # this ensures the entire script is downloaded before execution #
